9077c205003df94fd66f1c0a5baab6a5419a2692
agonzalez7702 <a.gonzalez7702@student.nu.edu>
2026-02-26 20:26:52 -0800

    Main

---
+++ Main.cpp
@@ -0,0 +1,23 @@
+/* ---------------------------------------------------------------------------
+** I attest that this following code represents my own work and is subject to
+** the plagiarism policy found in the course syllabus.
+**
+** Class: 	CSC 242
+** Assignment: 	Assignment 5: Comprehensive Program Challenge
+** File: 	Main.cpp
+** Description:
+**
+** Author: 	Alexandra Gonzalez
+** Date: 	2/25/26
+** -------------------------------------------------------------------------*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+
+
+	return 0;
+}
